19bcddc1634410b5753cc586eec9399f6a2c5a8f
Oghenero Ologe <nologe37@gmail.com>
2024-10-30 11:09:27 +0000

    added static file middleware

--- index.js
+++ index.js
@@ -8,4 +8,21 @@ app.use(function (req, res, next) {
   next();
 });
 
+// Static File Middleware
+app.use(function (req, res, next) {
+  const url = req.url;
+  const params = url.split("/").slice(1);
+  if (params[0] === "images") {
+    try {
+      const fileName = params.join("/");
+      const file = fs.readFileSync(fileName);
+      return res.end(file.toString());
+    } catch (e) {
+      return res.status(404).end("Image not found");
+      next();
+    }
+  }
+  next();
+});
+
 app.listen(3000, () => console.log("Listening on port 3000"));
