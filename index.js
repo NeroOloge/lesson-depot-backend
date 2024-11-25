e6f7b06bf51245c6cf5aadd8d71eeae4ad972060
Oghenero Ologe <neroologe37@gmail.com>
2024-11-25 16:57:32 +0000

    refactored PORT

--- index.js
+++ index.js
@@ -43,4 +43,6 @@ app.use(function (req, res, next) {
 
 app.use(apiRouter);
 
-app.listen(3000, () => console.log("Listening on port 3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log("Listening on port 3000"));
