7dd81a2fbd2f5c109ba092a26c7a0ab1084fc821
Oghenero Ologe <nologe37@gmail.com>
2024-11-25 14:22:49 +0000

    fixed issues with lessons update

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const apiRouter = require("./router/apiRouter");
 
 const app = express();
 
+app.use(express.json());
+
 app.use(cors());
 
 // Logging Middleware
--- router/apiRouter.js
+++ router/apiRouter.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { ObjectId } = require("mongodb");
 const { db } = require("../db");
 
 const router = express.Router();
@@ -11,8 +12,27 @@ router.get("/lessons", function (req, res, next) {
 router.post("/orders", function (req, res, next) {
   const order = req.body;
   // TODO: create order
-  db.collection("orders").insertOne(order);
-  return res.json({ msg: "Success" });
+  db.collection("orders")
+    .insertOne(order)
+    .then((result) => res.json({ msg: "Success" }))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ msg: "An error occurred" });
+    });
+});
+
+router.get("/search", function (req, res, next) {
+  const searchTerm = req.query.searchTerm;
+
+  const lessons = db.collection("lessons").find({
+    $or: [
+      { subject: { $regex: `${searchTerm}` } },
+      { location: { $regex: `${searchTerm}` } },
+      { price: { $regex: `${searchTerm}` } },
+      { spaces: { $regex: `${searchTerm}` } },
+    ],
+  });
+  lessons.toArray().then((results) => res.json(results));
 });
 
 router.put("/lessons/:id", function (req, res, next) {
@@ -20,7 +40,7 @@ router.put("/lessons/:id", function (req, res, next) {
   const updatedLesson = req.body;
   // TODO: update lesson
   db.collection("lessons").findOneAndUpdate(
-    { _id: id },
+    { _id: new ObjectId(id) },
     { $set: updatedLesson }
   );
   return res.json({ msg: "Success" });
