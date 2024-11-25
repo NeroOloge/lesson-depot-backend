4dea672cff2d1fe785f9f2da0f74b7fe7dac73c3
Oghenero Ologe <nologe37@gmail.com>
2024-11-25 15:28:00 +0000

    added image fetch functionality

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const apiRouter = require("./router/apiRouter");
@@ -20,11 +22,20 @@ app.use(function (req, res, next) {
   const params = url.split("/").slice(1);
   if (params[0] === "images") {
     try {
-      const fileName = params.join("/");
-      const file = fs.readFileSync(fileName);
-      return res.end(file.toString());
+      const fileName = path.join(__dirname, ...params);
+      if (!fs.existsSync(fileName)) {
+        res.writeHead(404, {
+          "Content-Type": "text/plain",
+        });
+        return res.end("Image not found");
+      }
+      return res.sendFile(fileName);
     } catch (e) {
-      return res.status(404).end("Image not found");
+      console.error(e);
+      res.writeHead(500, {
+        "Content-Type": "text/plain",
+      });
+      return res.end("Internal Server Error");
     }
   }
   next();
