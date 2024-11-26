567c46073e4e5f003ef778130522c86f3c153c7d
Oghenero Ologe <neroologe37@gmail.com>
2024-11-26 14:15:44 +0000

    changed listening confirmation log

--- index.js
+++ index.js
@@ -45,4 +45,4 @@ app.use(apiRouter);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log("Listening on port 3000"));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
